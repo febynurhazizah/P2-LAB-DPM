b75773bcc4e4cc05e4f3f2a6eee61f52e5402ddd
unknown <febbynurhazizah@gmail.com>
2024-12-02 15:39:55 +0700

    LAB DPM

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.centerText}>Ini Adalah Tugas 2 Praktikum DPM</Text>
+      <View style={styles.boxContainer}>
+        <View style={styles.box1}></View>
+        <View style={styles.box2}></View>
+      </View>
     </View>
   );
 }
@@ -13,8 +16,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#e8ebea',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  centerText: {
+    fontSize: 24,
+    color: 'blue',
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  boxContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+    
+  box1: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'pink',
+  },
+  box2: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#fc84d5',
   },
 });
